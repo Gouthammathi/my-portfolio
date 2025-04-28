5f4dab65cb2d410d95b216032cae965a19f9b0c6
gouthamm02 <goouthamm@gmail.com>
2025-04-28 23:48:45 +0530

    fourth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        sora: ['Sora', 'sans-serif'],
+        sora: ['Sora', 'sans-serif'], // 👈 Add this
       },
     },
   },
